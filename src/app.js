6022d12709bad68ee7d4f03fa583604f9620468f
Lars Krieger <lars.krieger@gmail.com>
2017-02-22 20:33:24 +0100

    Renamed file welcome.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Welcome from './views/Welcome';
+import Welcome from './views/welcome';
 
 ReactDOM.render(
   <Welcome name="World" />,
